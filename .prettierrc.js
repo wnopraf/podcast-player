78913fde0b594b3ca2e84e8b117f79e7545317e9
wnopraf <e.bcncgroup.tid@telefonica.com>
2026-05-01 13:22:37 +0200

    chore: add prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  endOfLine: 'lf',
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  embeddedLanguageFormatting: 'auto',
+};
