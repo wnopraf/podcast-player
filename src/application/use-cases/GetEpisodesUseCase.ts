8fd1446f7ab27732aa12cd9520a9f621f00ab0c3
wnopraf <e.bcncgroup.tid@telefonica.com>
2026-05-02 14:06:45 +0200

    feat: add GetEpisodesUseCase

---
+++ src/application/use-cases/GetEpisodesUseCase.ts
@@ -0,0 +1,16 @@
+import type { Episode } from '../../domain/entities/Episode';
+import type { EpisodeRepository } from '../../domain/ports/EpisodeRepository';
+import type { Result } from '../../shared/types/Result';
+import type { ApplicationError } from '../../shared/errors/ApplicationError';
+
+export class GetEpisodesUseCase {
+  private episodeRepository: EpisodeRepository;
+
+  constructor(episodeRepository: EpisodeRepository) {
+    this.episodeRepository = episodeRepository;
+  }
+
+  async execute(collectionId: string): Promise<Result<Episode[], ApplicationError>> {
+    return await this.episodeRepository.findByCollectionId(collectionId);
+  }
+}
