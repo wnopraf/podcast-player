675d14c7f2fa22b2d91d5d78dd46f01d384d4b3a
wnopraf <e.bcncgroup.tid@telefonica.com>
2026-05-09 12:01:40 +0200

    feat: replace di factory pattern by di container based in awilix

---
+++ src/infrastructure/di/container.ts
@@ -0,0 +1,33 @@
+import { createContainer, asClass } from 'awilix';
+import { PlayerService } from '@domain/services/PlayerService';
+import { AudioPlayerAdapter } from '@infrastructure/adapters/AudioPlayerAdapter';
+import { EpisodeQueueService } from '@domain/services/EpisodeQueueService';
+import { GetPodcastsUseCase } from '@application/use-cases/GetPodcastsUseCase';
+import { SearchPodcastsUseCase } from '@application/use-cases/SearchPodcastsUseCase';
+import { OrderPodcastsUseCase } from '@application/use-cases/OrderPodcastsUseCase';
+import { PodcastApiAdapter } from '@infrastructure/adapters/api/PodcastApiAdapter';
+import { GetEpisodesUseCase } from '@application/use-cases/GetEpisodesUseCase';
+import { EpisodeApiAdapter } from '@infrastructure/adapters/api/EpisodeApiAdapter';
+
+export const diContainer = createContainer();
+
+// Domain services
+diContainer.register({
+  audioPlayerAdapter: asClass(AudioPlayerAdapter).singleton(),
+  episodeQueueService: asClass(EpisodeQueueService).singleton(),
+  playerService: asClass(PlayerService).singleton(),
+});
+
+// Repositories
+diContainer.register({
+  podcastRepository: asClass(PodcastApiAdapter).singleton(),
+  episodeRepository: asClass(EpisodeApiAdapter).singleton(),
+});
+
+// Use cases
+diContainer.register({
+  getPodcastsUseCase: asClass(GetPodcastsUseCase).singleton(),
+  searchPodcastsUseCase: asClass(SearchPodcastsUseCase).singleton(),
+  orderPodcastsUseCase: asClass(OrderPodcastsUseCase).singleton(),
+  getEpisodesUseCase: asClass(GetEpisodesUseCase).singleton(),
+});
--- src/infrastructure/di/playerDi.ts
+++
@@ -1,9 +0,0 @@
-import { PlayerService } from '@domain/services/PlayerService';
-import { AudioPlayerAdapter } from '@infrastructure/adapters/AudioPlayerAdapter';
-import { EpisodeQueueService } from '@domain/services/EpisodeQueueService';
-
-export const createPlayerService = (): PlayerService => {
-  const audioPlayer = new AudioPlayerAdapter();
-  const queueService = new EpisodeQueueService();
-  return new PlayerService(audioPlayer, queueService);
-};
